bc04bd6bdfbdf5dea84485016e4e1ea7245d8574
Malcolm Vigren <trekommafem2@gmail.com>
2021-07-07 10:47:10 +0200

    Initial commit with duration module

---
+++ src/duration.py
@@ -0,0 +1,35 @@
+class Duration:
+    
+    def __init__(self, milliseconds, seconds=0, minutes=0):
+        self.milliseconds = milliseconds + seconds * 1000 + minutes * 60 * 1000
+
+    def minutes_seconds_milliseconds(self):
+        minutes = (self.milliseconds / 1000) // 60
+        seconds = (self.milliseconds - (minutes * 60 * 1000)) // 1000
+        milliseconds = self.milliseconds - seconds * 1000 - minutes * 60 * 1000
+        return int(minutes), int(seconds), int(milliseconds)
+
+    def __add__(self, other):
+        return Duration(self.milliseconds + other.milliseconds)
+
+    def __mul__(self, num):
+        return Duration(int(self.milliseconds * num))
+
+    def __eq__(self, other):
+        return self.milliseconds == other.milliseconds
+
+    def __ge__(self, other):
+        return self.milliseconds >= other.milliseconds
+
+    def __gt__(self, other):
+        return self.milliseconds > other.milliseconds
+
+    def __le__(self, other):
+        return self.milliseconds <= other.milliseconds
+
+    def __lt__(self, other):
+        return self.milliseconds < other.milliseconds
+
+    def __repr__(self):
+        minutes, seconds, milliseconds = self.minutes_seconds_milliseconds()
+        return f"Duration(minutes={minutes}, seconds={seconds}, milliseconds={milliseconds})"
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from src.duration import Duration
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
