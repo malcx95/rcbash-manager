0ebd8ae73e199eca44f388102b672e1da3898520
Malcolm Vigren <trekommafem2@gmail.com>
2022-12-12 19:55:53 +0100

    #18: Add add-button and make datalist suggestions work again

--- server/static/js-components/startlist.js
+++ server/static/js-components/startlist.js
@@ -1,5 +1,14 @@
 $(document).ready(() => {
 
+  /*
+   * TODO lägg till ett normal- och edit-läge.
+   * I normal-läget ska textrutan och alla redigeringskontroller vara
+   * borta/dolda, och en edit-knapp ska finnas. Klickar man på edit-knappen
+   * går man in i edit-läget. På så sätt kan denna användas på alla ställen
+   * där startlistor används. Naturligtvis ska editläget bara vara tillgängligt
+   * för administratörer.
+   */
+
 class StartListInput extends Component {
   constructor() {
     super();
@@ -12,65 +21,114 @@ class StartListInput extends Component {
       return;
     }
     const datalistId = this.getAttribute("datalist");
-    let driversDataList = $("#drivers")[0];
+    let driversDataList = document.getElementById(datalistId);
     this.allDrivers = this.getDriverDictionaryFromDatalist(driversDataList);
     // copy allDrivers to initialize availableDrivers
     this.availableDrivers = Object.assign({}, this.allDrivers);
+    /*
+     * TODO kanske gör en egen datalist som kan hålla koll på availableDrivers?
+     * Så flera startlists kan ta del av dem och synka med varandra?
+     */
 
     const rootDiv = document.createElement("div");
-    rootDiv.classList.add("table-responsive");
     this.shadow.appendChild(rootDiv);
 
+    this.createCSS(rootDiv);
+
     // Create the table element
+    const tableDiv = document.createElement("div");
+    tableDiv.classList.add("table-responsive");
+    rootDiv.appendChild(tableDiv);
+
     const table = document.createElement("table");
     table.classList.add("table", "table-striped", "table-sm");
-    rootDiv.appendChild(table);
+    tableDiv.appendChild(table);
 
     this.constructTableHeader(table);
 
     this.tableBody = document.createElement("tbody");
     table.appendChild(this.tableBody);
 
-    const inputBox = this.createInputBox(rootDiv, datalistId);
-    this.setUpCallbacks(inputBox);
+    this.createInputBox(rootDiv, datalistId, driversDataList);
 
-    this.populateTable();
+    this.updateState();
   }
 
-  createInputBox(rootDiv, datalistId) {
+  updateState() {
+    this.populateTable();
+    let numbers = this.drivers.map((d) => d.number);
+    console.log(numbers);
+    this.setAttribute("value", numbers);
+  }
+
+  createInputBox(rootDiv, datalistId, datalist) {
     const label = document.createElement("label");
     label.classList.add("form-label");
     label.textContent = "Lägg till förare";
     rootDiv.appendChild(label);
 
+    const inputDiv = document.createElement("div");
+    inputDiv.classList.add("input-div");
+    rootDiv.appendChild(inputDiv);
+
     const input = document.createElement("input");
     input.setAttribute("list", datalistId);
     input.setAttribute("type", "text");
     input.classList.add("form-control");
-    rootDiv.appendChild(input);
+    inputDiv.appendChild(input);
 
-    return input;
+    const addButton = document.createElement("button");
+    addButton.classList.add("btn", "btn-primary", "add-button");
+    addButton.textContent = "Lägg till";
+    addButton.disabled = true;
+    inputDiv.appendChild(addButton);
+
+    // add the datalist here again since it's not visible in the shadow DOM
+    rootDiv.appendChild(datalist);
+
+    this.setUpCallbacks(input, addButton);
   }
 
-  setUpCallbacks(inputBox) {
+  createCSS(rootDiv) {
+    const style = document.createElement("style");
+    style.textContent = `
+      .add-button {
+        width: 30%;
+        margin-left: 5px;
+      }
+      .input-div {
+        display: flex;
+      }
+    `;
+    rootDiv.appendChild(style);
+  }
+
+  setUpCallbacks(inputBox, addButton) {
 
     inputBox.onkeypress = (event) => {
       var keycode = (event.keyCode ? event.keyCode : event.which);
       if (keycode == '13') {
-        this.addInputToListIfValid(event.target, event.target.value);
+        this.addInputToListIfValid(event.target, event.target.value, addButton);
       }
     };
 
+    addButton.onclick = (event) => {
+      this.addInputToListIfValid(inputBox, inputBox.value, addButton);
+    };
+
+    inputBox.oninput = (event) => {
+      addButton.disabled = !(inputBox.value in this.availableDrivers);
+    };
   }
 
-  addInputToListIfValid(textbox, number) {
+  addInputToListIfValid(textbox, number, addButton) {
     if (number in this.availableDrivers) {
       this.drivers.push({name: this.availableDrivers[number], number: number});
       textbox.value = "";
-      this.populateTable();
+      addButton.disabled = true;
+      this.updateState();
     }
   }
-
 
   populateTable() {
     this.tableBody.textContent = "";
